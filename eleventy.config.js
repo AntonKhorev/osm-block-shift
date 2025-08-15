28d08c3cac96959d75c516d19b4182cc0a3d4d1c
Anton Khorev <tony29@yandex.ru>
2025-08-15 05:12:18 +0300

    Enable footnotes

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 import { HtmlBasePlugin } from "@11ty/eleventy"
+import markdownItFootnote from "markdown-it-footnote"
 
 export default function (eleventyConfig) {
+	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItFootnote))
 	eleventyConfig.addPlugin(HtmlBasePlugin)
 }
