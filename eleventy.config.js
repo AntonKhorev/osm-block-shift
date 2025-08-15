6716d93689ba422759a416a4225040eb7e6c0012
Anton Khorev <tony29@yandex.ru>
2025-08-15 05:48:04 +0300

    Configure footnotes

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,13 @@ import { HtmlBasePlugin } from "@11ty/eleventy"
 import markdownItFootnote from "markdown-it-footnote"
 
 export default function (eleventyConfig) {
-	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItFootnote))
+	eleventyConfig.amendLibrary("md", (md) => {
+		md.use(markdownItFootnote)
+		md.renderer.rules.footnote_block_open = () => (
+			`<section class="footnotes">\n` +
+			`<h2>Примечания</h2>\n` +
+			`<ol class="footnotes-list">\n`
+		)
+	})
 	eleventyConfig.addPlugin(HtmlBasePlugin)
 }
