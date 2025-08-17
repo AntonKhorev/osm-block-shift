9a0155ae1aa9f5428cbe469770f2a5f2c2b51a4f
Anton Khorev <tony29@yandex.ru>
2025-08-17 03:22:56 +0300

    Add TODO shortcode

--- eleventy.config.js
+++ eleventy.config.js
@@ -18,5 +18,6 @@ export default function (eleventyConfig) {
 			return `[\`${kv}\`](https://wiki.openstreetmap.org/wiki/Tag:${encodeURIComponent(kv)})`
 		}
 	})
+	eleventyConfig.addPairedShortcode("TODO", function () {})
 	eleventyConfig.addPlugin(HtmlBasePlugin)
 }
